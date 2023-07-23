a38becc8a313452f22ca04838a45198e3be116d2
sariszoom <sariszoom@gmail.comuyuftytyddtydtyfyufyufyuftyudtdghdghdfdfgdfffhjftyffdrtdtysrtdtydtydfrtsddghfghsddghhjghfghjgfghjfghfghlgkl;jdfklgjfkldgjldfkjglkfjgh;lkdgjhlkjfgf;dkds;fkjhjihkdfkjfdigjiodfjgl;lkpso>
2023-07-23 21:19:21 +0700

    classwork 4

--- class7/classwork4/laplaciantAndSobel.py
+++ class7/classwork4/laplaciantAndSobel.py
@@ -1,37 +1,67 @@
 import numpy as np
 import cv2 as cv
 
-def sobel_filter(image):
-    # Sobel kernel แบบ Horizontal
-    sobel_horizontal = np.array([[-1, -2, -1],
-                                 [0, 0, 0],
-                                 [1, 2, 1]])
+def FourierTransform(img):
+    img32 = img.astype(np.float32)  # เปลี่ยนภาพให้เป็น float32
+    imgFourier = np.fft.fft2(img32)  # คืนเป็นภาพ Spectrum
+    imgFourier = np.fft.fftshift(imgFourier) #เลื่อนศูนย์ของภาพมาอยู่ตรงกลาง
+    # for magnitude spectrum
+    imgReal = np.real(imgFourier)  # แยกส่วนจริงของ Fourier-transformed image
+    imgImag = np.imag(imgFourier)  # แยกส่วนจินตภาพของ Fourier-transformed image
+    imgMag = np.sqrt(imgReal**2 + imgImag**2)  #  คำนวณสเปกตรัมของภาพโดยใช้สมการ Magnitude = sqrt(Re^2 + Im^2)
+    imgMag = np.log(1 + imgMag)  # เพิ่มความชัดเจนในการแสดงผลภาพ
+    imgMag = cv.normalize(imgMag, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)  # ปรับค่าสเกลของภาพให้สามารถแสดงผลภาพได้
 
+    return imgFourier, imgMag
+
+
+if __name__ == '__main__': # 1. นำเข้าไลบรารี OpenCV และ NumPy
+    
+    img = cv.imread('class7/img/catonnote.jpg', 0) #แปลงให้เป็นภาพขาวด้วยพารามิเตอร์เป็น 0
+    h, w = img.shape 
+    
     # Sobel kernel แบบ Vertical
     sobel_vertical = np.array([[-1, 0, 1],
                                [-2, 0, 2],
                                [-1, 0, 1]])
+    
+    kernel_length = len(sobel_vertical) #3
+    
+    #sobel filter in frequency domain
+    
+    #1.เติมพื้นที่ของ kernel เพื่อให้ขนาดของ kernel เท่ากับขนาดของภาพที่ผ่านการคำนวณ Fourier ทำให้ convolution ในโดเมนความถี่ได้
+    kernel_pad = np.pad(sobel_vertical, ((h // 2 , h // 2 - kernel_length), (w // 2 , w // 2 - kernel_length)), mode='constant')
+    kernel = np.fft.fftshift(kernel_pad)
 
-    # Filtering แบบ Horizontal และ Vertical
-    filtered_horizontal = cv.filter2D(image, -1, sobel_horizontal)
-    filtered_vertical = cv.filter2D(image, -1, sobel_vertical)
+    # 2. คำนวณ Fourier transform ทั้ง kernel และ image โดยใช้ฟังก์ชัน FourierTransform
+    filterF, filterMag = FourierTransform(kernel)
+    imgF, imgMag = FourierTransform(img)
+    cv.imwrite('class7/out/imgMag.png', imgMag)
+    cv.imwrite('class7/out/filterMag.png', filterMag)
 
-    return filtered_horizontal, filtered_vertical
+    # 3. คำนวณผลคูณ (dot product) ของภาพที่ผ่านการคำนวณ Fourier-transform และ kernel 
+    dotProduct = imgF * filterF  
 
-# อ่านภาพเข้ามา
-image = cv.imread('class7/img/catonnote.jpg', cv.IMREAD_GRAYSCALE)
 
-# ใช้ Sobel Filter แบบ Horizontal และ Vertical กับภาพ
-filtered_horizontal, filtered_vertical = sobel_filter(image)
+    # 4. ทำการส่วนกลับของ Fourier transform ให้กับผลคูณเพื่อของรูปที่ได้ผ่านการกรอง
+    imgF_filtered = np.fft.ifftshift(dotProduct)
+    imgF_filtered = np.fft.ifft2(imgF_filtered)
+    filtered = np.real(imgF_filtered)
+    imgF_filtered = cv.normalize(filtered, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)  # Normalize the filtered image
+    cv.imwrite('class7/out/Sobel-Frequency.png', imgF_filtered)
 
-# ทำ Fourier Transform
-fft_image = np.fft.fft2(image)
-# fft_shifted = np.fft.fftshift(fft_image)
 
-# แสดงผลลัพธ์
-cv.imwrite('class7/classwork4/sobelx.png', filtered_vertical)
-cv.imwrite('class7/classwork4/sobely.png',filtered_horizontal)
-cv.imwrite('class7/classwork4/fft2.png',image)
-# cv.imwrite('class7/classwork4/fftshift.png',fft_shifted)
-cv.waitKey(0)
-cv.destroyAllWindows()
+
+# Sobel Filter ใน Spatial domain
+
+    sobelX = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=3)
+    sobelX = cv.normalize(sobelX, None, 0, 255, cv.NORM_MINMAX, cv.CV_8U)
+    cv.imwrite('class7/out/Sobel-Spatial.png', sobelX)
+
+    # เปรียบเทียบ 2 โดเมน
+    compareImage = np.concatenate((imgF_filtered, sobelX), axis=1)
+
+
+    cv.imshow('Frequency and Spatial domain', compareImage)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
